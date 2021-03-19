2c8264a0c155dfca346ed128f552ed9c6de5e040
James Duarte <GarnetSunset@users.noreply.github.com>
2021-03-19 18:22:48 -0400

    lessfuckingo

--- getAndPrepUpdate.py
+++ getAndPrepUpdate.py
@@ -1,4 +1,4 @@
-import os, sys, requests,shutil,zipfile, io
+import os, sys, requests,shutil,zipfile, io, eboot_translator
 from distutils.dir_util import copy_tree
 
 if not os.path.exists("pkgsForUpdate"):
@@ -7,15 +7,13 @@ if not os.path.exists("pkgsForUpdate"):
 FNULL = open(os.devnull, 'w') 
 
 scetool = "https://cdn.discordapp.com/attachments/489871388879093780/822550726655213578/scetool.zip"
-
+decrypt = "https://cdn.discordapp.com/attachments/732351773687414825/822592241922867240/decrypt.zip"
 digital = ["http://b0.ww.np.dl.playstation.net/tppkg/np/NPJB00532/NPJB00532_T7/24b46000c027864a/JP0177-NPJB00532_00-RYUISHINRETAIL00-A0104-V0100-PE.pkg","http://b0.ww.np.dl.playstation.net/tppkg/np/NPJB00532/NPJB00532_T7/24b46000c027864a/JP0177-NPJB00532_00-RYUISHINRETAIL00-A0105-V0100-PE.pkg","http://b0.ww.np.dl.playstation.net/tppkg/np/NPJB00532/NPJB00532_T7/24b46000c027864a/JP0177-NPJB00532_00-RYUISHINRETAIL00-A0106-V0100-PE.pkg","http://b0.ww.np.dl.playstation.net/tppkg/np/NPJB00532/NPJB00532_T7/24b46000c027864a/JP0177-NPJB00532_00-RYUISHINRETAIL00-A0107-V0100-PE.pkg","http://b0.ww.np.dl.playstation.net/tppkg/np/NPJB00532/NPJB00532_T7/24b46000c027864a/JP0177-NPJB00532_00-RYUISHINRETAIL00-A0108-V0100-PE.pkg"]
-digitalEboot = "7e539669"
 disc = ["http://b0.ww.np.dl.playstation.net/tppkg/np/BLJM61149/BLJM61149_T8/b2eeb24f08b90ce1/JP0177-BLJM61149_00-GAMEVER0104WEEK4-A0104-V0100-PE.pkg","http://b0.ww.np.dl.playstation.net/tppkg/np/BLJM61149/BLJM61149_T8/b2eeb24f08b90ce1/JP0177-BLJM61149_00-GAMEVER0105WEEK5-A0105-V0100-PE.pkg","http://b0.ww.np.dl.playstation.net/tppkg/np/BLJM61149/BLJM61149_T8/b2eeb24f08b90ce1/JP0177-BLJM61149_00-GAMEVER0106WEEK6-A0106-V0100-PE.pkg","http://b0.ww.np.dl.playstation.net/tppkg/np/BLJM61149/BLJM61149_T8/b2eeb24f08b90ce1/JP0177-BLJM61149_00-GAMEVER0107WEEK7-A0107-V0100-PE.pkg","http://b0.ww.np.dl.playstation.net/tppkg/np/BLJM61149/BLJM61149_T8/b2eeb24f08b90ce1/JP0177-BLJM61149_00-GAMEVER0108WEEK8-A0108-V0100-PE.pkg"]
 
 
-if os.path.exists("\pkgsForUpdate\PSN_get_pkg_info.py") == False:
-    r = requests.get("https://raw.githubusercontent.com/windsurfer1122/PSN_get_pkg_info/master/PSN_get_pkg_info.py", allow_redirects=True)
-    open("pkgsForUpdate/PSN_get_pkg_info.py", 'wb').write(r.content)
+r = requests.get("https://raw.githubusercontent.com/windsurfer1122/PSN_get_pkg_info/master/PSN_get_pkg_info.py", allow_redirects=True)
+open("pkgsForUpdate/PSN_get_pkg_info.py", 'wb').write(r.content)
 
 r = requests.get(scetool)
 z = zipfile.ZipFile(io.BytesIO(r.content))
@@ -44,10 +42,21 @@ for file in os.listdir("pkgsForUpdate"):
             copy_tree(os.getcwd()+"/pkgsForUpdate/update/"+directory_contents[0], os.getcwd()+"/patch/")
             shutil.rmtree(os.getcwd()+"/pkgsForUpdate/update/"+directory_contents[0])   
 #insert eboot decryption and modification
+
 copy_tree(os.getcwd()+"/mainDIR/", os.getcwd()+"/patch/")
 files_in_directory = os.listdir(os.getcwd()+"/patch/")
+os.chdir("pkgsForUpdate")
+if discOrDigital == "B":
+    os.system("scetool -d ../patch/USRDIR/EBOOT.BIN ../patch/USRDIR/EBOOT.BIN")
+else:
+    r = requests.get(decrypt)
+    z = zipfile.ZipFile(io.BytesIO(r.content))
+    z.extractall(".")
+    os.system("decrypt_eboot.exe ../patch/USRDIR/EBOOT.BIN ../patch/USRDIR/EBOOT.BIN JP0177-NPJB00532_00-RYUISHINRETAIL00.rap")
+    
+os.chdir("..")
 filtered_files = [file for file in files_in_directory if file.endswith(".psd")]
 for file in filtered_files:
 	path_to_file = os.path.join(os.getcwd()+"/patch/", file)
 	os.remove(path_to_file)
-shutil.rmtree(os.getcwd()+"/pkgsForUpdate/")
+#shutil.rmtree(os.getcwd()+"/pkgsForUpdate/")
