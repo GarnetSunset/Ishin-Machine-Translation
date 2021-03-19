c92057a027558e9ed90eeb4be4de07873fd18c8a
James Duarte <GarnetSunset@users.noreply.github.com>
2021-03-19 15:35:44 -0400

    patches stuff

--- getAndPrepUpdate.py
+++ getAndPrepUpdate.py
@@ -1,8 +1,13 @@
-import os, sys
-import requests
+import os, sys, requests
+import shutil
+from distutils.dir_util import copy_tree
 
 if not os.path.exists("pkgsForUpdate"):
     os.makedirs("pkgsForUpdate")
+
+FNULL = open(os.devnull, 'w') 
+
+scetool = "https://cdn.discordapp.com/attachments/489871388879093780/822550726655213578/scetool.zip"
 
 digital = ["http://b0.ww.np.dl.playstation.net/tppkg/np/NPJB00532/NPJB00532_T7/24b46000c027864a/JP0177-NPJB00532_00-RYUISHINRETAIL00-A0104-V0100-PE.pkg","http://b0.ww.np.dl.playstation.net/tppkg/np/NPJB00532/NPJB00532_T7/24b46000c027864a/JP0177-NPJB00532_00-RYUISHINRETAIL00-A0105-V0100-PE.pkg","http://b0.ww.np.dl.playstation.net/tppkg/np/NPJB00532/NPJB00532_T7/24b46000c027864a/JP0177-NPJB00532_00-RYUISHINRETAIL00-A0106-V0100-PE.pkg","http://b0.ww.np.dl.playstation.net/tppkg/np/NPJB00532/NPJB00532_T7/24b46000c027864a/JP0177-NPJB00532_00-RYUISHINRETAIL00-A0107-V0100-PE.pkg","http://b0.ww.np.dl.playstation.net/tppkg/np/NPJB00532/NPJB00532_T7/24b46000c027864a/JP0177-NPJB00532_00-RYUISHINRETAIL00-A0108-V0100-PE.pkg"]
 digitalEboot = "7e539669"
@@ -12,8 +17,6 @@ disc = ["http://b0.ww.np.dl.playstation.net/tppkg/np/BLJM61149/BLJM61149_T8/b2ee
 if os.path.exists("\pkgsForUpdate\PSN_get_pkg_info.py") == False:
     r = requests.get("https://raw.githubusercontent.com/windsurfer1122/PSN_get_pkg_info/master/PSN_get_pkg_info.py", allow_redirects=True)
     open("pkgsForUpdate/PSN_get_pkg_info.py", 'wb').write(r.content)
-
-import PSN_get_pkg_info
     
 discOrDigital = input("Do you have the bluray version or digital version of Ishin?\nType \"B\" for BluRay, type \"D\" for Digital\n> ")
 
@@ -31,5 +34,10 @@ else:
 
 for file in os.listdir("pkgsForUpdate"):
     if file.endswith(".pkg"):
-        os.system("echo Hello from the other side!")
-        print(os.path.join(file))
+        with open(os.devnull, 'wb') as devnull:
+            os.system("python "+os.getcwd()+"/pkgsForUpdate/PSN_get_pkg_info.py "+os.getcwd()+"/pkgsForUpdate/"+file+" --content "+os.getcwd()+"/pkgsForUpdate/update")
+            os.remove(os.getcwd()+"/pkgsForUpdate/"+file)
+            directory_contents = os.listdir(os.getcwd()+"/pkgsForUpdate/update/")
+            copy_tree(os.getcwd()+"/pkgsForUpdate/update/"+directory_contents[0], os.getcwd()+"/patch/")
+            shutil.rmtree(os.getcwd()+"/pkgsForUpdate/update/"+directory_contents[0])   
+shutil.rmtree(os.getcwd()+"/pkgsForUpdate/update/")
