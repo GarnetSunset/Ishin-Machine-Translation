1a4576c568004a9f5c3e9dc3d583568961133e7a
Timo654 <harrikarris654@gmail.com>
2021-03-20 14:10:27 +0200

    Fixed quotes in eboot translation and improved translator script.

--- eboot_translator.py
+++ eboot_translator.py
@@ -1,54 +1,59 @@
 import struct
-import sys
+import argparse
+import os
 import pandas as pd
 
-
-def write_string(data, offset, string, ignoreLength):
+def write_string(data, offset, string, ignore_length):
     pos = offset
     end = data[pos:].index(b'\x00')
 
     i = 0
     while i < len(data[pos+end:]) and data[pos+end:][i] == 0:
         i += 1
-    text_file = open("FIXUS.txt", "w")
+    text_file = open("FIXUS.txt", "a")
+
     max_len = end + i - 1
     try:
         byte_string = string.encode("shift_jisx0213").replace(b'[n]', b'\x0A')
-        if len(byte_string) > max_len and ignoreLength == False:
-            print(f"Text is too long - offset: {offset}, translation: {string}, max length: {max_len}")
-            text_file.write(f"Text is too long - offset: {offset}, translation: {string}, max length: {max_len}")
-        elif len(byte_string) = 0:
-            print(f"Text missing - offset: {offset}, translation: {string}, max length: {max_len}")
+        if len(byte_string) > max_len and not ignore_length:
+            print(f"Text is too long - offset: {hex(offset)}, translation: {string}, max length: {max_len}")
+            text_file.write(f"Text is too long - offset: {hex(offset)}, translation: {string}, max length: {max_len} \n")
+        elif len(byte_string) == 0:
+            print(f"Text missing - offset: {hex(offset)}, translation: {string}, max length: {max_len}")
+            text_file.write(f"Text missing - offset: {hex(offset)}, translation: {string}, max length: {max_len} \n")
         else:
             #print(string)
             struct.pack_into(f"{max_len}s", data, pos, byte_string)
     except(TypeError):
-            print(f"Wrong type - offset: {offset}, translation: {string}, max length: {max_len}")
-            text_file.write(f"Wrong type - offset: {offset}, translation: {string}, max length: {max_len}")
+        print(f"Wrong type - offset: {hex(offset)}, translation: {string}, max length: {max_len}")
+        text_file.write(f"Wrong type - offset: {hex(offset)}, translation: {string}, max length: {max_len} \n")
     except(AttributeError):
-            print(f"Wrong type - offset: {offset}, translation: {string}, max length: {max_len}")    
-            text_file.write(f"Wrong type - offset: {offset}, translation: {string}, max length: {max_len}")    
+        print(f"Wrong type - offset: {hex(offset)}, translation: {string}, max length: {max_len}")    
+        text_file.write(f"Wrong type - offset: {hex(offset)}, translation: {string}, max length: {max_len} \n")    
     except(UnicodeEncodeError):
-            byte_string = string.encode("shift_jisx0213").replace(b'[n]', b'\x0A')
-            if len(byte_string) < max_len:
-                struct.pack_into(f"{max_len}s", data, pos, byte_string)
+        byte_string = string.encode("shift_jisx0213").replace(b'[n]', b'\x0A')
+        if len(byte_string) < max_len:
+            struct.pack_into(f"{max_len}s", data, pos, byte_string)
     text_file.close()
 
 
-def replace_strings(origfile,eboot,ignoreLength):
+def replace_strings(text, eboot, ignore_length, output):
     with open(eboot, "rb") as f:
         data = bytearray(f.read())
 
+    if os.path.exists('FIXUS.txt'): #deletes old log if it exists
+        os.remove('FIXUS.txt')
+
     #csv file with text
-    original_text = origfile
-    df = pd.read_csv(original_text, delimiter=';')
+    translated_text = text
+    df = pd.read_csv(translated_text, delimiter=';')
     offsets = df.iloc[:, 0]
     strings = df.iloc[:, 1]
 
     for o, s in zip(offsets, strings):
-        write_string(data, int(o, 16), s,ignoreLength)
+        write_string(data, int(o, 16), s, ignore_length)
     
-    with open(eboot, "wb") as f:
+    with open(output, "wb") as f:
         f.write(data)
 
 
@@ -74,8 +79,20 @@ def print_strings(eboot):
 
 
 def main():
-    sys.stdout = open('log.txt', 'w')
-    replace_strings()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input",  help='Eboot file (EBOOT.elf)', type=str)
+    parser.add_argument("text", help="Pick a csv file containing text and offsets.", type=str)
+    parser.add_argument("output", help="Output eboot name.", type=str)
+
+    parser.add_argument("-il,", "--ignorelength", help="Ignores length.") #why does this even exist?
+    args = parser.parse_args()
+
+    if args.ignorelength:
+        ignore_length = True
+    else:
+        ignore_length = False
+        
+    replace_strings(args.text, args.input, ignore_length, args.output)
 
 
 if __name__ == "__main__":
